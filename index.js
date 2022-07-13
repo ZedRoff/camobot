b1dd2fee5e80b467e940f7c025d032a8ca8a3971
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-07-14 00:56:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,35 +3,67 @@ const allIntents = new Discord.Intents(32767)
 const bot = new Discord.Client({ intents: allIntents})
 const config = require("./utils/config.json")
 const fs = require("fs")
+const funcs = require("./utils/errors.js")
 
 bot.commands = {}
 
-
 fs.readdir('./commands/', (err, files) => {
     files.forEach(file => {
-        const f = require(`./commands/${files}`);
+        const f = require(`./commands/${file}`);
         let cmd = file.split(".")[0]
-        bot.commands[cmd] = [f.command, f.infos.description, f.infos.aliases]
+        try {
+            bot.commands[cmd] = [f.command, f.infos.description, f.infos.aliases]
+        }catch(e) {
+            console.log("erreur")
+        }
+        
 
     })
-   
 })
+
 let prefix = config.prefix
 
 bot.once("ready", () => {
     console.log("Bot prêt !")
     bot.user.setActivity("En développement", {type: "WATCHING"})
+    bot.user.setStatus("dnd")
 })
 
 bot.on("messageCreate", message => {
     if(message.author.bot) return;
-    let args_strip = message.content.split(" ")
-    let cmd = args_strip[0]
-    let name_cmd = cmd.split(prefix)[1]
-   let args = args_strip.slice(1)
+        let args_strip = message.content.split(" ")
+        let cmd = args_strip[0]
+        let name_cmd = cmd.split(prefix)[1]
+        let args = args_strip.slice(1)
 
+    if(cmd == prefix + "info") {
+   
+    if(!args[0]) return funcs.errorArgs(Discord,message,prefix,"[prefix]info [nom_de_la_commande]")
+
+    if(bot.commands[args[0]] !== undefined) {
+        let embed = new Discord.MessageEmbed()
+        .setColor("#00FFFF")
+        .setTitle(`Page d'aide de la commande ${args[0]}`)
+        .setDescription(`Description : ${bot.commands[args[0]][1]}`)
+        
+        message.channel.send({embeds: [embed]})
+    }else {
+        let embed = new Discord.MessageEmbed()
+        .setColor("#FF0000")
+        .setDescription("Cette commande n'a pas été trouvée")
+
+        message.channel.send({embeds: [embed]})
+    }
+  
+   }
     if(bot.commands[name_cmd] !== undefined) {
-        bot.commands[name_cmd][0](bot, message, args)
+        try {
+            console.log("capted")
+            bot.commands[name_cmd][0](bot, message, args)
+        }catch(e) {
+            message.channel.send("Erreur, merci de contacter le créateur du bot par mp : `</ZedRoff>#6268`")
+            console.log(e)
+        } 
     }
 })
 bot.login(config.token)
