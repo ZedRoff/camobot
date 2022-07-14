fb3be768eb998def3c8b8bfd0f4e49fd1aac11f4
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-07-14 22:58:06 +0200

    Update errors.js

--- utils/errors.js
+++ utils/errors.js
@@ -1,8 +1,25 @@
-exports.errorArgs = (Discord, message, prefix, msg) => {
+exports.errorArgs = (Discord, message, msg) => {
     let embed = new Discord.MessageEmbed()
     .setColor("#FF0000")
     .setTitle("Problème d'argument")
-    .setDescription(msg.replace("[prefix]", prefix))
+    .setDescription(msg)
 
     message.channel.send({embeds: [embed]})
+}
+
+exports.negativeInteger = (Discord, message ) => {
+    let embed = new Discord.MessageEmbed()
+    .setColor("#FF0000")
+    .setTitle("Mauvais nombre en entré")
+    .setDescription("Les nombres négatifs ne sont pas autorisées dans cette commande")
+
+    message.channel.send({ embeds: [embed]})
+}
+exports.wrongEntry = (Discord, message, msg) => {
+    let embed = new Discord.MessageEmbed()
+    .setColor("#FF0000")
+    .setTitle("Mauvaise entrée")
+    .setDescription(msg)
+
+    message.channel.send({ embeds: [embed]})
 }
