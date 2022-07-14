816ebe2af87d9e66395083fdd91a994fad434399
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-07-14 22:56:37 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,13 +4,15 @@ const config = require("../utils/config.json")
 exports.command = (bot, message,args) => {
 
     let general = ["test", "help"]
+    let fun = ["roll"]
     let embed = new Discord.MessageEmbed()
     .setColor("#7F56FF")
     .setTitle("Commande d'aide du bot")
     .setDescription("Si vous souhaitez avoir plus d'informations sur une commande spécifique, merci d'utiliser la commande `info`")
-    .addFields({
-        name: "Commandes générales", value: funcs.encadrer(general.join(" | "))
-    })
+    .addFields(
+        {name: "Commandes Générales", value: funcs.encadrer(general.join(" | "))},
+        {name: "Commandes Fun", value: funcs.encadrer(fun.join(" | "))}
+    )
     .setFooter({text: `Version : ${config.version}`, iconURL: bot.user.displayAvatarURL()})
 
     message.channel.send({embeds: [embed]})
@@ -18,5 +20,6 @@ exports.command = (bot, message,args) => {
 exports.infos = {
     name: "help",
     description:"Page d'aide des commandes du bot",
-    aliases: ["h"]
+    aliases: ["h"],
+    usage: "[prefix]help"
 }
