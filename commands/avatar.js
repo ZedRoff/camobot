1bc7cfaa9cad61c356abcfb83cf1548b67d1790a
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-07-15 21:54:35 +0200

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js")
+
+exports.command = (bot,  message, args) => {
+    let user = message.mentions.users.first() || bot.users.cache.get(args[0]) || message.author;
+    let embed = new Discord.MessageEmbed()
+    .setColor("DARKER_GREY")
+    .setTitle("L'image de profile de : "+user.username)
+    .setImage(user.displayAvatarURL())
+
+    message.channel.send({ embeds: [embed] })
+}
+
+exports.infos = {
+    name: "avatar",
+    usage: "[prefix]avatar (user)",
+    description: "Affiche l'avatar d'un utilisateur",
+    alias: []
+}
