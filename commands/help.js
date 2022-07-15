558f1abfdce1443f26fa876553e01206790c9e84
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-07-15 17:53:51 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -3,15 +3,18 @@ const funcs = require("../utils/md.js")
 const config = require("../utils/config.json")
 exports.command = (bot, message,args) => {
 
-    let general = ["test", "help"]
-    let fun = ["roll"]
+    let general = ["help", "botinfo", "macro"]
+    let fun = ["roll", "lovecalc"]
+    let mods = ["eval", "macro"]
     let embed = new Discord.MessageEmbed()
     .setColor("#7F56FF")
     .setTitle("Commande d'aide du bot")
     .setDescription("Si vous souhaitez avoir plus d'informations sur une commande spécifique, merci d'utiliser la commande `info`")
     .addFields(
         {name: "Commandes Générales", value: funcs.encadrer(general.join(" | "))},
-        {name: "Commandes Fun", value: funcs.encadrer(fun.join(" | "))}
+        {name: "Commandes Fun", value: funcs.encadrer(fun.join(" | "))},
+        {name: "Commande de modération", value: funcs.encadrer(mods.join(" | "))}
+
     )
     .setFooter({text: `Version : ${config.version}`, iconURL: bot.user.displayAvatarURL()})
 
