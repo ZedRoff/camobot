31dcf36cff762d2194431d494ed047c95c603c0f
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-07-16 22:56:57 +0200

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,28 @@
+const Discord = require("discord.js")
+const funcs = require("../utils/errors")
+const embed_f = require("../utils/embed")
+
+exports.command = (bot, message, args) => {
+if(!args[0]) return funcs.errorArgs(Discord, message, "[prefix]say [mode] [message]")
+let choices = ["normal", "embed"]
+let mode = args[0]
+if(!choices.includes(mode)) return embed_f.embedMaker(Discord, message, '#FF0000', "Le mode n'est pas le bon, (normal et embed supportés uniquement)")
+
+if(!args[1]) return funcs.errorArgs(Discord, message, "[prefix]say [mode] [message]")
+let msg = args.slice(1).join(" ")
+switch(mode) {
+    case "normal": {
+        message.channel.send(args.slice(1).join(" "))
+    }
+    break;
+    case "embed": {
+        return embed_f.embedMaker(Discord, message, '#00FFFF', msg)
+    }
+}
+}
+exports.infos = {
+    name: "say",
+    description: "Fait parler le bot a votre place",
+    usage:"[prefix]say [mode] [message]",
+    alias: []
+}
