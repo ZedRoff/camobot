840543bfd93b309944f67d1419fc826142fa17ca
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-07-19 00:48:23 +0200

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,7 +1,8 @@
 const Discord = require("discord.js")
-
+const emb = require("../utils/embed")
 exports.command = (bot,  message, args) => {
     let user = message.mentions.users.first() || bot.users.cache.get(args[0]) || message.author;
+    if(!user) return emb.embedMaker(Discord, message, '#00FFFF', "L'utilisateur n'a pas été trouver.")
     let embed = new Discord.MessageEmbed()
     .setColor("DARKER_GREY")
     .setTitle("L'image de profile de : "+user.username)
