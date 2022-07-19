9fef0bc16fea248d68d30a8131cde596c0ea1ec9
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-07-19 20:19:13 +0200

    Create account.js

---
+++ models/account.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+const model = new mongoose.Schema({
+    user_id: String,
+    user_name: String,
+    user_rep: Number,
+    user_badge: Array,
+    user_cmd_count: Number,
+    user_description: String,
+    user_bg: String,
+    user_color: String,
+    user_birth_date: String
+
+})
+
+module.exports = mongoose.model("Account_milleka", model)
