d4fa1278051cee69838b343b558077db5ef66267
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-07-22 01:13:50 +0200

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -2,6 +2,7 @@ const Discord = require("discord.js")
 const translate = require('translate-google')
 const funcs = require("../utils/errors")
 const emb = require("../utils/embed")
+const md = require("../utils/md")
 
 exports.command =  (bot, message, args) => {
 
@@ -10,7 +11,7 @@ exports.command =  (bot, message, args) => {
 if(!args[0]) return funcs.errorArgs(Discord, message, "[prefix]translate [langue] [texte] (langue manquante)")
 if(!args[1]) return funcs.errorArgs(Discord, message, "[prefix]translate [langue] [texte] (texte manquant)")
 translate(args.slice(1).join(" "), {to: args[0]}).then(res => {
-message.channel.send(res)
+message.channel.send(md.encadrer(res))
 }).catch(err => {
     return emb.embedMaker(Discord, message, "#FF0000", "Une erreur est survenue, merci de vérifier si la langue entrée est bonne (exemple: pour le français ça devient fr, pour l'anglais c'est en etc ...")
 })
