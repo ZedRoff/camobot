524d89b94713016a93d8fe24987f688ddcf61efb
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-07-21 17:04:17 +0200

    Create a_template.js

---
+++ commands/a_template.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js")
+const md = require("../utils/md")
+const funcs = require("../utils/errors")
+let emb = require("../utils/embed")
+
+exports.command = async (bot, message, args) => {
+
+ }
+exports.infos = {
+    name: "",
+    description: "",
+    alias: [],
+    usage: ""
+}
+
