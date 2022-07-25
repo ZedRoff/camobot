8ce91fca4885326c316851f7964242e391b60c13
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-07-25 18:27:33 +0200

    Update unregister.js

--- commands/unregister.js
+++ commands/unregister.js
@@ -6,7 +6,6 @@ const emb = require("../utils/embed")
 exports.command = (bot, interaction, options) => {
  
     model.deleteOne({user_id: interaction.member.user.id}, (err, doc) => {
-       if(err) return interaction.reply({content: "Une erreur est survenue, merci de contacter le créateur du bot </ZedRoff>#6268"})
         if(doc.deletedCount !== 1) {
             return interaction.reply({embeds: [emb.embedMaker(Discord, message, '#FF0000', "Tu n'as pas de compte")]})
         }else {
