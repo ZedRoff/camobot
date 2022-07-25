1a428ab5b3a4ec393e765d7c5c9acc5e8c76e4ae
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-07-25 18:30:02 +0200

    setup config and cds

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const config = require("./utils/config.json")
 const fs = require("fs")
 const funcs = require("./utils/errors.js")
 const mongoose = require("mongoose")
-const model_prefix = require("./models/prefix")
+
 const embed_f = require("./utils/embed")
 const model_afk = require("./models/afk")
 const md = require("./utils/md")
@@ -47,7 +47,21 @@ fs.readdir('./commands/', (err, files) => {
 
 bot.once("ready", () => {
 
-   
+   const g = bot.guilds.cache.get("765920191967526932")
+   let commands;
+   if(g) {
+    commands = g.commands;
+   } else {
+    commands = bot.application.commands
+   }
+  
+   fs.readdir("./commands", (err, files) => {
+    files.forEach(file => {
+        const f = require(`./commands/${file}`)
+        commands.create(f.infos)
+    })
+   })
+  
  
     console.log("Bot prêt !")
 
@@ -66,11 +80,79 @@ bot.once("ready", () => {
     
     bot.user.setStatus("dnd")
 })
-// Afk system
+
+bot.on("interactionCreate", async interaction => {
+        if(!interaction.isCommand()) return;
+        const { commandName, options } = interaction
+        
+        fs.readdir("./commands", (err, files) => {
+            files.forEach(file => {
+                const f = require(`./commands/${file}`)
+        
+                if(f.infos.name == commandName) {
+                 if( cooldown.has(interaction.member.user.id)) return interaction.reply({content: "Tu es sous cooldown, tu dois attendre 5 secondes"})
+              
+                 try {
+
+              
+                    f.command(bot, interaction, options)
+                }catch(e) {
+                    message.channel.send(`Une erreur est survenue, merci de contacter le créateur du bot ${config.owner}`)
+                    console.log(e)
+                }
+                if(interaction.member.user.id == config.ownerID) return;
+                cooldown.add(interaction.member.user.id)
+                    setTimeout(() => {
+                        cooldown.delete(interaction.member.user.id)
+                    }, 5000)
+                }
+            })
+        })
+        
+})
+
 
 bot.on("messageCreate", message => {
     if(message.author.bot) return;
-    if(message.channel.type == "DM") return embed_f.embedMaker(Discord, message, '#00FFFF', "Merci d'utiliser les services que ce bot propose en MP")
+    if(message.channel.type == "DM") return message.channel.send({embeds: [embed_f.embedMaker('#00FFFF', "Merci d'utiliser les services que ce bot propose en MP")]})
+
+
+
+    let args_strip = message.content.split(" ")
+    let cmd = args_strip[0]
+
+    let prefix = "/"
+  
+    let name_cmd = cmd.split(prefix)[1]
+    let args = args_strip.slice(1)
+
+
+if(cmd == prefix + "info") {
+
+    // TODO : have to convert this part to full indexed searching
+if(!args[0]) return message.channel.send({embeds: [funcs.errorArgs("[prefix]info [nom_de_la_commande]")]})
+
+if(bot.commands[args[0]] !== undefined) {
+    let embed = new Discord.MessageEmbed()
+    .setColor("#00FFFF")
+    .setTitle(`Page d'aide de la commande ${args[0]}`)
+    .setDescription("Merci de ne pas inclure les `[] et ()` dans la commande.\nLégende : `[]` = requis, `()` = optionnel.")
+    .addFields({name: "Description :", value: bot.commands[args[0]][2]}, {name: "Usage", value: bot.commands[args[0]][3].replace("[prefix]", prefix)}, 
+     {name: "Alias", value: bot.commands[args[0]][4].length == 0 ? "Pas d'alias" : bot.commands[args[0]][4].join(" | ")})
+ 
+    message.channel.send({embeds: [embed]})
+}else {
+    let embed = new Discord.MessageEmbed()
+    .setColor("#FF0000")
+    .setDescription("Cette commande n'a pas été trouvée")
+
+    message.channel.send({embeds: [embed]})
+}
+
+}
+
+
+
     if(!message.content.includes("<@")) return;
     model_afk.find({}, (err, doc) => {
         doc.map(info => {
@@ -81,100 +163,10 @@ bot.on("messageCreate", message => {
            
         })
     })
-    console.log(message.content)
-})
-
-// End afk system
-
-bot.on("messageCreate", message => {
-    if(message.author.bot) return;
-    if(message.channel.type == "DM") return embed_f.embedMaker(Discord, message, '#00FFFF', "Merci d'utiliser les services que ce bot propose en MP")
-        let args_strip = message.content.split(" ")
-        let cmd = args_strip[0]
-
-        let prefix = ""
-        model_prefix.findOne({guild_id: message.guild.id}, (err, doc) => {
-            if(!doc) {
-                const new_prefix = new model_prefix({
-                    guild_id: message.guild.id,
-                    prefix: "²"
-                })
-                new_prefix.save()
-                prefix = "²"
-            } else {
-                prefix = doc.prefix
-                
-            }
-             
-        let name_cmd = cmd.split(prefix)[1]
-        let args = args_strip.slice(1)
-
-
-
-
-
-    if(cmd == prefix + "info") {
    
-        // TODO : have to convert this part to full indexed searching
-    if(!args[0]) return funcs.errorArgs(Discord,message,prefix,"[prefix]info [nom_de_la_commande]")
-
-    if(bot.commands[args[0]] !== undefined) {
-        let embed = new Discord.MessageEmbed()
-        .setColor("#00FFFF")
-        .setTitle(`Page d'aide de la commande ${args[0]}`)
-        .setDescription("Merci de ne pas inclure les `[] et ()` dans la commande.\nLégende : `[]` = requis, `()` = optionnel.")
-        .addFields({name: "Description :", value: bot.commands[args[0]][2]}, {name: "Usage", value: bot.commands[args[0]][3].replace("[prefix]", prefix)}, 
-         {name: "Alias", value: bot.commands[args[0]][4].length == 0 ? "Pas d'alias" : bot.commands[args[0]][4].join(" | ")})
-     
-        message.channel.send({embeds: [embed]})
-    }else {
-        let embed = new Discord.MessageEmbed()
-        .setColor("#FF0000")
-        .setDescription("Cette commande n'a pas été trouvée")
-
-        message.channel.send({embeds: [embed]})
-    }
-  
-   }
-
-
-   let keys = Object.keys(bot.commands)
-   let values = Object.values(bot.commands)
-   for(let i=0;i<keys.length;i++) {
     
-       if(keys[i] == name_cmd || values[i][4].includes(name_cmd)) {
-        try {
-      
-        if(cooldown.has(message.author.id)) return message.reply("Hep hep, calme sur le spam des commandes")
+})
 
-        const model_profile = require("./models/account")
-        model_profile.findOne({user_id: message.author.id}, (err, doc) => {
-            if(!doc) return;
-            doc.user_cmd_count++
-            doc.save()
-        })
-        values[i][0](bot, message, args)
-        if(message.author.id == '327074335238127616') return;
-        cooldown.add(message.author.id)
-       
-        setTimeout(() => {
-            cooldown.delete(message.author.id)
-        }, 3000)
-        }catch(e) {
-            message.channel.send("`Une erreur est survenue, merci de contacter le créateur du bot </ZedRoff>#6268`")
-            console.log(e)
-        }
-       }
-      
-   }
-      
-        
- 
-})
    
-   
-       
-})
+  
 bot.login(config.token)
-
-// TODO : change all aliases to alias on github
