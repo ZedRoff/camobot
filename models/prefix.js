56c7f3dce33500d1bc701681b67821acb8fcf524
ZedRoff <48176184+ZedRoff@users.noreply.github.com>
2022-07-26 18:32:37 +0200

    Delete prefix.js

--- models/prefix.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-
-const model = new mongoose.Schema({
-    guild_id: String,
-    prefix: String
-})
-
-module.exports = mongoose.model("Milleka_prefix", model)
